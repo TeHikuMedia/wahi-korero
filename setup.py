dd077e49d51725913a526a01186a20ec0b67958c
Keoni Mahelona <keoni@kmahelona.net>
2022-10-25 20:18:10 +1300

    license and setup for pip install

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='wahi-korero',
+    version='v0.5.3',
+    description='A tool for identifying and extracting segments of speech in audio.',
+    url='https://github.com/TeHikuMedia/wahi-korero',
+    author='@craigthelinguist, @kmahelona',
+    author_email='info@tehiku.nz',
+    license='Kaitiakitanga License',
+    packages=['wahi_korero'],
+    install_requires=[
+        'pydub==0.22.1',
+        'webrtcvad==2.0.10',
+    ],
+)
